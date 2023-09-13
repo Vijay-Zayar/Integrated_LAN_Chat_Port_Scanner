7bc8e9668d33d15b123742bbc0dbcc0ad9570e18
Z@Y@R <61421901+Vijay-Zayar@users.noreply.github.com>
2023-09-14 00:24:34 +0630

    Modified UI and added leave functions

---
+++ client.py
@@ -0,0 +1,174 @@
+import socket
+import threading
+import datetime
+import ttkbootstrap as ttk
+import menus
+
+HOST = '192.168.0.11'
+PORT = 1234
+connected_status = False
+client_socket = None
+
+PRIMARY_FONT = "Helvetica"
+
+def chatClient(right_frame):
+    # Clear the right frame
+    for widget in right_frame.winfo_children():
+        widget.destroy()
+
+    inner_frame = ttk.Frame(right_frame)
+    inner_frame.pack(pady=10)
+
+    label = ttk.Label(inner_frame, text="Client Mode", font=(PRIMARY_FONT, 20))
+    label.grid(row=0, column=0, columnspan=3, pady=10)
+
+    hostIP_label = ttk.Label(inner_frame, text="Host IP", font=(PRIMARY_FONT, 16))
+    hostIP_entry = ttk.Entry(inner_frame, font=(PRIMARY_FONT, 16), bootstyle="INFO", width=36)
+    hostIP_label.grid(row=1, column=0, padx=8, pady=25)
+    hostIP_entry.grid(row=1, column=1, pady=10)
+
+    user_label = ttk.Label(inner_frame, text="User Name", font=(PRIMARY_FONT, 16))
+    user_entry = ttk.Entry(inner_frame, font=(PRIMARY_FONT, 16), bootstyle="INFO", width=36)
+    # user_entry.bind("<Return>", lambda: connectToServer())
+    join_btn = ttk.Button(inner_frame, text="Join", bootstyle="INFO", width=9, command=lambda: connectToServer())
+    user_label.grid(row=2, column=0, pady=15)
+    user_entry.grid(row=2, column=1, padx=8)
+    join_btn.grid(row=2, column=2, padx=10)
+
+    message_area = ttk.ScrolledText(inner_frame, font=("Arial", 12),  width=57, height=20)
+    message_area.grid(row=3, column=0, columnspan=3, pady=20)    
+    message_area.config(state=ttk.DISABLED)
+
+    message_entry = ttk.Entry(inner_frame, font=(PRIMARY_FONT, 16), bootstyle="SUCCESS", width=58)
+    # message_entry.bind("<Return>", lambda: sendMessage)
+    message_entry.grid(row=4, column=0, columnspan=3)
+    
+    send_btn = ttk.Button(inner_frame, text="Send", bootstyle="SUCCESS", width=9, command=lambda: sendMessage())
+    leave_btn = ttk.Button(inner_frame, text="Leave", bootstyle="WARNING", width=9, command=lambda: leaveChat())
+    # leave_btn = ttk.Button(inner_frame, text="Leave", bootstyle="WARNING", width=9, command=lambda: menus.chooseClientServer(right_frame))
+    send_btn.grid(row=5, column=1, padx=7, pady=20, sticky="e")
+    leave_btn.grid(row=5, column=2)
+
+    message_box = ttk.dialogs.dialogs.Messagebox
+    
+    # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def addMessage(message):
+        message_area.config(state = ttk.NORMAL)
+        message_area.insert(ttk.END, message + '\n')
+        message_area.config(state = ttk.DISABLED)
+
+
+    def clearMessages():
+        message_area.config(state = ttk.NORMAL)
+        message_area.delete(1.0, ttk.END)  # Delete from the start (1.0) to the end (ttk.END)
+        message_area.config(state = ttk.DISABLED)
+
+
+    def connectToServer():
+        global HOST
+        global client_socket
+
+        username = user_entry.get().strip()
+        HOST = hostIP_entry.get().strip()
+
+        if username != '' and HOST != '':
+            try:
+                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                client_socket.connect((HOST, PORT))
+                client_socket.sendall(username.encode())
+                threading.Thread(target=listen_for_messages_from_server, args=(client_socket, )).start()
+                
+                global connected_status
+                connected_status = True
+
+                print("Successfully connected to server")
+                addMessage("[SERVER] Successfully connected to the server")
+
+                hostIP_entry.config(state = ttk.DISABLED)
+                user_entry.config(state = ttk.DISABLED)
+                join_btn.config(state = ttk.DISABLED)
+
+            except:
+                message_box.show_error("Unable to connect to server !", "Connection Error", parent=hostIP_entry)
+                hostIP_entry.config(state = ttk.NORMAL)
+                user_entry.config(state = ttk.NORMAL)
+                join_btn.config(state = ttk.NORMAL)
+
+        else:
+            message_box.show_warning("Host IP and User Name cannot be empty !", "Invalid Input", parent=hostIP_entry)
+
+
+    def sendMessage():
+        global client_socket
+
+        if connected_status:
+            message = message_entry.get()
+
+            if message != '':
+                client_socket.sendall(message.encode())
+
+            else:
+                message_box.show_warning("Message cannot be empty !", "Invalid Input", parent=hostIP_entry)
+
+            message_entry.delete(0, len(message))
+        
+        else:
+            message_box.show_warning("Join to the server first !", "Warning", parent=hostIP_entry)
+
+    
+    def listen_for_messages_from_server(client_socket):
+        try:
+            while 1:
+                time = datetime.datetime.now().time()
+                message = client_socket.recv(2048).decode('utf-8')
+
+                if not message:
+                    break  # Server closed the connection gracefully
+
+                if message != '':
+                    username = message.split("~")[0]
+                    content = message.split('~')[1]
+
+                    addMessage(f"[{time.hour}:{time.minute}:{time.second}] [{username}] {content}")
+
+                else:
+                    message_box.show_warning("Message recevied from client is empty !", "Warning", parent=hostIP_entry)
+                    
+        except ConnectionAbortedError:
+            # Handle the case where the connection is abruptly closed by the server
+            print("Connection to the server was abruptly closed.")
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+        finally:
+            # Close the client socket when the thread exits
+            client_socket.close()
+
+
+    def leaveChat():
+        global client_socket
+
+        if connected_status:
+            client_socket.sendall("LEAVE".encode('utf-8'))
+            
+            # Close the client_socket
+            client_socket.close()
+            
+            # Update the GUI to indicate that the user is leaving
+            addMessage("[SERVER] You have left the chat")
+
+            hostIP_entry.config(state = ttk.NORMAL)
+            hostIP_entry.delete(0, len(hostIP_entry.get()))
+
+            user_entry.config(state = ttk.NORMAL)
+            user_entry.delete(0, len(user_entry.get()))
+
+            join_btn.config(state = ttk.NORMAL)
+            
+            clearMessages()
+        
+        else:
+            message_box.show_warning("Cannot leave from chat !", "Warning", parent=hostIP_entry)
---
+++ server.py
@@ -0,0 +1,212 @@
+import socket
+import threading
+import ttkbootstrap as ttk
+import menus
+
+PRIMARY_FONT = "Helvetica"
+
+HOST = ''
+PORT = 1234 # You can use any port between 0 to 65535
+LISTENER_LIMIT = 30
+active_clients = [] # List of all currently connected users
+
+# initial_server = True
+server_status = False
+server_thread = None
+server_socket = None
+
+def runServer(right_frame):
+    # Clear the right frame
+    for widget in right_frame.winfo_children():
+        widget.destroy()
+
+    inner_frame = ttk.Frame(right_frame)
+    inner_frame.pack(pady=20)
+
+    label = ttk.Label(inner_frame, text="Server Mode", font=(PRIMARY_FONT, 20))
+    label.grid(row=0, column=0, columnspan=2, pady=25)
+
+    start_btn = ttk.Button(inner_frame, text="Start", bootstyle="SUCCESS", width=10, command=lambda: start_server(label))
+    stop_btn = ttk.Button(inner_frame, text="Stop", bootstyle="WARNING", width=10, command=lambda: stop_server(label))
+    # stop_btn = ttk.Button(inner_frame, text="Stop", bootstyle="WARNING", width=10, command=lambda: menus.chooseClientServer(right_frame))
+    start_btn.grid(row=1, column=0, padx=10, pady=30)
+    stop_btn.grid(row=1, column=1, padx=10)
+
+    message_box = ttk.dialogs.dialogs.Messagebox
+
+
+    def start_server(parent_frame):
+        # global initial_server
+        global server_status
+        global server_thread
+        global server_socket
+
+        try:
+            if server_thread is None or not server_thread.is_alive():
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # Getting Server IP Address
+                # server_ip = get_local_ip()
+                server_ip = "172.16.0.2"
+                HOST = server_ip
+                print(f"Server IP Address: {server_ip}")
+
+                try:
+                    server_socket.bind((HOST, PORT))
+                    print(f"Running the server on {HOST} {PORT}")
+                    message_box.show_info(f"Running the server on {HOST} : {PORT}","Server Started", parent=parent_frame)
+
+                except:
+                    print(f"Unable to bind to host {HOST} and port {PORT}")
+                    
+                # Set server limit
+                server_socket.listen(LISTENER_LIMIT)
+
+                server_status = True
+
+                server_thread = threading.Thread(target=accept_connections)
+                server_thread.start()
+                # initial_server = False
+                print("Server has started successfully")
+
+        except:
+            # initial_server = True
+            message_box.show_error("Unable to start the server !", "Connection Error", parent=parent_frame)
+
+
+    def stop_server(parent_frame): 
+        global active_clients
+        global server_status
+        global server_thread
+        global server_socket
+        
+        if len(active_clients) == 0 and server_socket and server_thread.is_alive():
+            server_status = False
+
+            server_socket.close()
+            server_socket = None
+            # server_thread.join()
+
+            print("Server has stopped successfully")
+            message_box.show_info("Server has stopped successfully", "Server Success", parent=parent_frame)
+
+        else: 
+            message_box.show_warning("Unable to stop ! Users are online", "Server Warning", parent=parent_frame)
+
+
+    # Function to send message to a single client
+    def send_message_to_client(client, message):
+
+        client.sendall(message.encode()) 
+
+
+    # Function to send any new message to all the clients that
+    # are currently connected to this server
+    def send_messages_to_all(message):
+        global active_clients
+        
+        for user in active_clients:
+
+            send_message_to_client(user[0], message)
+
+
+    # Function to listen for upcoming messages from a client
+    def listen_for_messages(client, address, username):
+        global active_clients
+
+        while server_status:
+            try:
+                message = client.recv(2048).decode('utf-8')
+
+                if (not message):
+                    message = ''
+                    break
+
+                if message == 'LEAVE':
+                    # Remove the leaving client from the list of active clients
+                       
+                        active_clients.remove((client, address, username))
+
+                        # Notify other clients that this user is leaving
+                        leave_message = "SERVER~" + f"{username} has left the chat"
+                        send_messages_to_all(leave_message)
+
+                        try: 
+                            # Close the client socket
+                            print("Before closing client socket")
+                            client.close()
+                            print("After closing client socket")
+                        except:
+                            print("Error Closing client socket") 
+                    
+
+                        break
+
+                if message != '':               
+                    final_msg = username + '~' + message
+                    send_messages_to_all(final_msg)
+
+                else:
+                    print(f"The message send from client {username} is empty")
+                    break
+
+            except ConnectionResetError:
+                # Handle the case where the client disconnects abruptly
+                # Remove the client from the list of active clients
+                active_clients.remove((client, address, username))
+
+                # Close the client socket
+                client.close()
+                break
+
+
+    # Function to handle client
+    def client_handler(client, address):
+        global active_clients
+        
+        # Server will listen for client message that will
+        # Contain the username
+        while server_status:
+            try: 
+                username = client.recv(2048).decode('utf-8')
+
+                if username != '':
+                    active_clients.append((client, address, username))
+                    prompt_message = "SERVER~" + f"{username} has joined the chat"
+                    send_messages_to_all(prompt_message)
+                    # listen_for_messages(client, username)
+                    break
+
+                else:
+                    print("Client username is empty")
+
+            except:
+                    active_clients.remove((client, address, username))
+                    client.close()
+                    break
+
+        threading.Thread(target=listen_for_messages, args=(client, address, username, )).start()
+
+
+    def accept_connections():
+
+        # This while loop will keep listening to client connections
+        while server_status:
+
+            try:
+                client, address = server_socket.accept()
+                print(f"Successfully connected to client {address[0]} {address[1]}")
+
+                threading.Thread(target=client_handler, args=(client, address, )).start()
+            
+            except:
+                pass
+
+
+    def get_local_ip():
+        # Get the local IP address of the machine
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        return local_ip
+
+    # if not server_thread is None:
+    #     server_thread.join()
